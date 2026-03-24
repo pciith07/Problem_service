c12834b612180bd82d237e276019f9d7c994a99d
Prashans Chauhan <prashans2308@gmail.com>
2026-03-24 21:31:48 +0530

    WIP: saving before switching

--- src/controllers/problems.controller.js
+++ src/controllers/problems.controller.js
@@ -1,25 +1,37 @@
+const {StatusCodes} = require('http-status-codes')
+
 function pingController(req,res){
     return res.json({message: "Ping Controller is up"})
 }
 
 function addProblem(req, res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message: 'Not implemented'
+    })
 }
 
 function getAllProblems(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message: 'Not implemented'
+    })
 }
 
 function getProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message: 'Not implemented'
+    })
 }
 
 function updateProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message: 'Not implemented'
+    })
 }
 
 function deleteProblem(req,res){
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message: 'Not implemented'
+    })
 }
 
 module.exports = {
